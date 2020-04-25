f5526d1ee5ab0be0137b6c9672c9ce7e2bdcfc39
spikegd <spikegdofficial@gmail.com>
2020-04-25 15:28:35 -0700

    Working alerts

--- common/util.js
+++ common/util.js
@@ -57,27 +57,49 @@ function startWatcher(bot) {
 }
 
 function doCheck(bot, i) {
-  if (i+1 < bot.watchlist.length) {
+  if (i < bot.watchlist.length) {
     var obj = bot.watchlist[i]
     amazon.details(bot, obj.link).then(item => {
-      if (obj.lastPrice < (parseFloat(item.price) || 0)) sendPriceAlert(bot, obj, item)
+      var price = item.price.replace(/^\D+/g, "")
+
+      if (obj.lastPrice > (parseFloat(price) || 0)) sendPriceAlert(bot, obj, item)
+      if (obj.lastPrice < (parseFloat(price) || 0)) pushPriceChange(bot, obj, item)
     })
 
     setTimeout(() => doCheck(bot, i + 1), 2000)
   }
+
+  bot.con.query(`SELECT * FROM watchlist`, (err, rows) => {
+    if (err) throw err
+    bot.watchlist = JSON.parse(JSON.stringify(rows))
+    console.log('Watchlist Updated!')
+  })
 }
 
 /**
  * Sends an alert to the guildChannel specified in the DB entry
  * 
- * TODO: Maybe support multiple alerts (out of stock, back in stock, etc.)
+ * TODO: Maybe support multiple alerts (out of stock, back in stock, etc.)?
  */
 function sendPriceAlert(bot, obj, item) {
-  var channel = bot.channels.cache.get(obj.channel)
+  var channel = bot.channels.cache.get(obj.channel_id)
   var embed = new MessageEmbed()
     .setTitle(`Price alert for ${item.full_title}`)
     .setAuthor(item.seller)
     .setDescription(`Old Price: $${obj.lastPrice}\nNew Price: ${item.price}`)
 
   if(channel) channel.send(embed)
+
+  pushPriceChange(bot, obj, item)
+}
+
+/**
+ * Pushes a change in price to the DB
+ */
+function pushPriceChange(bot, obj, item) {
+  var price = item.price.replace(/^\D+/g, "")
+  bot.con.query(`UPDATE watchlist SET lastPrice=? WHERE link=?`, [(parseFloat(price) || 0), obj.link], (err) => {
+    if (err) throw err
+    console.log("Price in DB updated!")
+  })
 }
