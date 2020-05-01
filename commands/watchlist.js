6e6ebc741f273f0f9a6a479192562efbe3df6bf7
spikegd <spikegdofficial@gmail.com>
2020-05-01 00:29:05 -0700

    Handle 0 items

--- commands/watchlist.js
+++ commands/watchlist.js
@@ -15,7 +15,7 @@ function run(bot, guild, message, args) {
       var links = rows.map((x, i) => `${i+1}. ${trim(x.item_name, 100)}\n${x.link.substring(0, x.link.lastIndexOf('/')) + '/'}`)
       var embed = new MessageEmbed()
         .setTitle('List of Amazon items currently being watched')
-        .setDescription(links.join('\n\n'))
+        .setDescription(links.length > 0 ? links.join('\n\n'):'You haven\'t added any items yet!')
         .setColor('BLUE')
         .setFooter(`Currently watching ${rows.length} links in this server`)
 
