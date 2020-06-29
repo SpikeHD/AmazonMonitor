2035426ef25c3aebd70bb23e82bf4d7a92e1946a
spikegd <spikegdofficial@gmail.com>
2020-06-28 20:39:43 -0700

    Fix wording

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = (bot, guild, message, args) => {
   return new Promise((resolve, reject) => {
     var embed = new MessageEmbed()
       .setTitle('AmazonMonitor: Commands and Help')
-      .setDescription('This will describe each function and what it does.\n Some commands take a hot second while, but 90% of the time it isn\'t broken, so don\'t worry')
+      .setDescription('This will describe each function and what it does.\n Some commands take a hot second, but 90% of the time it isn\'t broken, so don\'t worry')
       .setColor('RED')
     
     bot.commands.forEach(c => {
