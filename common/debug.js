27e7aa40132407331aa8fce13157e132f9af5ddc
SpikeHD <spikegdofficial@gmail.com>
2021-01-13 13:26:35 -0800

    Debug logger formatting

--- common/debug.js
+++ common/debug.js
@@ -4,16 +4,16 @@ const { debug_enabled } = require('../config.json')
 exports.log = (message, type = 'debug') => {
   let t
   switch(type.toLowerCase()) {
-  default:
-  case 'log': t = '[LOG] '.blue
-    break
-  case 'debug': t = '[DEBUG] '.green
-    break
-  case 'warn': t = '[WARNING] '.yellow
-    break
-  case 'error': t = '[ERROR] '.red
-    break
-  case 'info': t = '[MESSAGE] '.magenta
+    default:
+    case 'log': t = '[LOG] '.blue
+      break
+    case 'debug': t = '[DEBUG] '.green
+      break
+    case 'warn': t = '[WARNING] '.yellow
+      break
+    case 'error': t = '[ERROR] '.red
+      break
+    case 'info': t = '[MESSAGE] '.magenta
   }
 
   if(debug_enabled) console.log(t, message)
