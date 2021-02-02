9d0b5b862523c603ca8c0668d36da1c85e68ff8b
spikegd <spikegdofficial@gmail.com>
2021-02-02 01:58:35 -0800

    More simple testing

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,6 @@
 const util = require('../common/util')
+const pup = require('puppeteer')
+const cheerio = require('cheerio')
 
 describe('utils test', () => {
   it('tests price formatting', () => {
@@ -8,4 +10,37 @@ describe('utils test', () => {
     expect(util.priceFormat('$1,000.99') === '1000.99').toBeTruthy()
     expect(util.priceFormat('€1.000,99') === '1000.99').toBeTruthy()
   })
+
+  it('tests argument parser', () => {
+    let argsObj = {
+      link: ''
+    }
+
+    expect(util.argParser(['--link', 'one'], argsObj).link === 'one').toBeTruthy()
+    expect(util.argParser(['-l', 'two'], argsObj).link === 'two').toBeTruthy()
+  })
+
+  it('tests string trimming', () => {
+    expect(util.trim('12345', 10).length === 5).toBeTruthy()
+    expect(util.trim('123456', 2).length === 5).toBeTruthy()
+  })
+
+  it('tests URLParams', () => {
+    const params = {
+      'one': '1',
+      'two': '2'
+    }
+
+    expect(util.parseParams(params) === '?one=1&two=2&').toBeTruthy()
+  })
+
+  it('tests startPup', () => {
+    pup.launch = jest.fn()
+
+    util.startPup()
+
+    expect(pup.launch).toHaveBeenCalled()
+  })
+
+  // Complex stuff I have yet to do goes here
 })
